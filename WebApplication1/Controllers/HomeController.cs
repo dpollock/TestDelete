16b523ca513ee685614f9dacb1d824ce90a5ee95
Daniel Pollock <daniellpollock@gmail.com>
2016-06-27 08:32:44 -0500

    Changed message per requirements.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            var message = "Hi";
+            var message = "Adjusted Hi";
             return View();
         }
 
